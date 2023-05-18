93264b3bae563220666bf60e28e5f11ed094d905
JulienAganze <aganzejulien2017@gmail.com>
2023-05-19 00:10:33 +0200

    trying data ingestion into postgres

---
+++ week_2_data_ingestion/airflow/dags_new/data_ingestion_local.py
@@ -0,0 +1,42 @@
+
+import os
+
+from airflow import DAG
+
+from datetime import datetime
+
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
+
+
+local_workflow = DAG(
+    "LocalIngestionDag",
+    schedule_interval="0 6 2 * *",
+    start_date=datetime(2022,1,1)
+)
+
+
+
+#url = '/yellow_tripdata_2021-01.parquet'
+
+URL_PREFIX='https://d37ci6vzurychx.cloudfront.net/trip-data'
+URL_TEMPLATE= URL_PREFIX + '/yellow_tripdata_{{ execution_date.strftime(\'%Y-%m\') }}.parquet'
+OUTPUT_FILE_TEMPLATE = AIRFLOW_HOME +  '/output_{{ execution_date.strftime(\'%Y-%m\') }}.parquet'
+
+with local_workflow:
+
+    wget_task = BashOperator(
+        task_id='wget',
+        bash_command=f'curl -sSL {URL_TEMPLATE} > {OUTPUT_FILE_TEMPLATE}'
+        #bash_command='echo " {{ execution_date.strftime(\'%Y-%m\') }}"'
+    )
+
+    ingest_task = BashOperator(
+        task_id='ingest',
+        bash_command=f'ls {AIRFLOW_HOME}'
+    )
+
+    wget_task >> ingest_task
+    
